35110c7f3c037737f0f9320ae767edc51f4e05b5
Igor Potceluev <sam@nicetu.spb.ru>
2011-01-27 17:04:00 +0300

    Убрал типы booleanBit

--- extra/ortsTypes/ortsTypes.h
+++ extra/ortsTypes/ortsTypes.h
@@ -41,17 +41,4 @@ extern BYTE lenOrtsTypes[];
 typedef errType (*funcPtr)(void*);
 typedef errType (*funcVoid)(void);
 
-typedef struct {
-    BYTE value: 1;
-} __attribute__ ((packed)) booleanBit;
-
-typedef union {
-    struct {
-	BYTE value_0:1;
-	BYTE value_1:1;
-    } fields;
-    
-    BYTE value: 2;
-} __attribute__ ((packed)) boolean2Bits;
-
 #endif // ORTS_TYPES_H
