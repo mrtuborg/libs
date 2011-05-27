ef8c19f42bc510f60dcc910410da633d7dd47aa9
Vladimir A. Nosenko <nosenko@nicetu.spb.ru>
2011-05-27 16:48:18 +0400

    Definded to new classes of operations: action requests & conditional requests

---
+++ schedule/job/actionReq.h
@@ -0,0 +1,28 @@
+/*
+ * actionReq.h
+ *
+ *  Created on: May 20, 2011
+ *      Author: tuborg
+ */
+
+#ifndef ACTIONREQ_H_
+#define ACTIONREQ_H_
+
+#include "job.h"
+
+class actionReq: public job
+{
+public:
+	actionReq(const job_type &header, const rcsCmd& cmd);
+	actionReq(const BYTE* encode_array);
+	actionReq();
+	virtual ~actionReq();
+
+
+private:
+
+	int encode(BYTE*);
+	int decode(BYTE*);
+};
+
+#endif /* ACTIONREQ_H_ */
